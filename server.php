ea44dc3282c56002e3531f4889bed303168511d9
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2025-06-10 17:45:15 -0400

    remove the code that was encapsulated in server class from server file

--- server.php
+++ server.php
@@ -7,10 +7,3 @@ $port = '9080';
 
 $server = new Server($address, $port);
 $server->listen();
-
-while (true) {
-    $client = socket_accept($socket);
-    $response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World!";
-    socket_write($client, $response);
-    socket_close($client);
-}
